108622f1d5a1f606b8c91d100f7ce4b517bad47c
HeraldoArman <heraldoarman7@gmail.com>
2026-02-19 12:01:21 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/yomubackendjava/YomuBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomubackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
