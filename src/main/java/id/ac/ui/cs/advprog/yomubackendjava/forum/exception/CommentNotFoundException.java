b138d9b184a2e3577638cda38349d7e316288cc4
rizkyantariksa <rizkyantariksa02@gmail.com>
2026-03-06 19:43:57 +0700

    refactor: extend common exceptions instead of RuntimeException

--- src/main/java/id/ac/ui/cs/advprog/yomubackendjava/forum/exception/CommentNotFoundException.java
+++ src/main/java/id/ac/ui/cs/advprog/yomubackendjava/forum/exception/CommentNotFoundException.java
@@ -1,11 +1,10 @@
 package id.ac.ui.cs.advprog.yomubackendjava.forum.exception;
 
-import java.util.UUID;
+import id.ac.ui.cs.advprog.yomubackendjava.common.exception.NotFoundException;
 
-public class CommentNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
+public class CommentNotFoundException extends NotFoundException {
 
-    public CommentNotFoundException(UUID commentId) {
+    public CommentNotFoundException(java.util.UUID commentId) {
         super("Komentar dengan ID " + commentId + " tidak ditemukan");
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/yomubackendjava/forum/exception/UnauthorizedCommentAccessException.java
+++ src/main/java/id/ac/ui/cs/advprog/yomubackendjava/forum/exception/UnauthorizedCommentAccessException.java
@@ -1,7 +1,8 @@
 package id.ac.ui.cs.advprog.yomubackendjava.forum.exception;
 
-public class UnauthorizedCommentAccessException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
+import id.ac.ui.cs.advprog.yomubackendjava.common.exception.ForbiddenException;
+
+public class UnauthorizedCommentAccessException extends ForbiddenException {
 
     public UnauthorizedCommentAccessException() {
         super("Anda tidak memiliki izin untuk mengubah komentar ini");
